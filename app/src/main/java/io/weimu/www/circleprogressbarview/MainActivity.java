7035c85827c1df7e234b9032fe76c210f954cdc9
艹羊 <779213209@qq.com>
2016-11-16 00:55:47 +0800

    增加注释～

--- app/src/main/java/io/weimu/www/circleprogressbarview/MainActivity.java
+++ app/src/main/java/io/weimu/www/circleprogressbarview/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //手动开启动画
         final CircleProgressBarView cir01 = (CircleProgressBarView) findViewById(R.id.crv_01);
         new Handler().postDelayed(new Runnable() {
             @Override
